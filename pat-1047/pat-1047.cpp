047a4eef395ce6131397ebf6ec3c5c038d6600d4
CymbaWang <CymbaWang@gmail.com>
2021-01-26 17:34:40 +0800

    tle

---
+++ pat-1047/pat-1047.cpp
@@ -0,0 +1,47 @@
+﻿
+#include<iostream>
+#include<algorithm>
+#include<map>
+#include<set>
+using namespace std;
+int main()
+{
+	int N, K;
+	cin >> N >> K;
+	map<int, set<string>> stuList;
+	for (int i = 0; i < N; i++)
+	{
+		char stuName[5];
+		scanf("%s", stuName, 5);
+		int courNum;
+		scanf("%d", &courNum);
+		for (int j = 0; j < courNum; j++)
+		{
+			int courId;
+			scanf("%d", &courId);
+			if (stuList.find(courId) != stuList.end())
+			{
+				stuList[courId].insert(stuName);
+			}
+			else
+			{
+				set<string> vec = { stuName };
+				stuList[courId] = vec;
+			}
+		}
+	}
+	for (int i = 0; i < K; i++)
+	{
+		printf("%d %d\n", i + 1, stuList[i + 1].size());
+		int j = 0;
+		for (auto it : stuList[i + 1])
+		{
+			printf("%s", &it[0]);
+			if (j != stuList[i + 1].size() - 1 || i != K - 1)
+				printf("\n");
+			j++;
+		}
+	}
+
+	return 0;
+}
