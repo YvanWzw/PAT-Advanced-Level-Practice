0454eefee422ebeb2cd822f93462d274c7b57287
CymbaWang <CymbaWang@gmail.com>
2021-02-03 22:07:54 +0800

    ac

---
+++ pat-1019/pat-1019.cpp
@@ -0,0 +1,40 @@
+﻿
+#include<iostream>
+#include<algorithm>
+#include<vector>
+#include<string>
+using namespace std;
+
+int main()
+{
+	int val = 0;
+	int base = 0;
+	cin >> val;
+	cin >> base;
+	vector<int> vec;
+	do {
+		int t = val % base;
+		vec.push_back(t);
+		val /= base;
+	} while (val != 0);
+	bool flag = false;
+	for (int i = 0; i < vec.size() / 2; i++)
+		if (vec[i] != vec[vec.size() - i - 1])
+			flag = true;
+
+	if (flag == false)
+		cout << "Yes" << endl;
+	else
+		cout << "No" << endl;
+	for (int i = vec.size() -1; i >=0; i--)
+	{
+		if (i != 0)
+			cout << vec[i] << " ";
+		else
+			cout << vec[i];
+	}
+
+
+	system("pause");
+	return 0;
+}
